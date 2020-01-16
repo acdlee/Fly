09589cd7f0f2c58c0dfaf8a1f07437e5939dec55
Christopher Lee <christopherlee@wireless-10-104-179-111.umd.edu>
2020-01-16 18:27:33 -0500

    Added test cases for airport system to test_airports.py; consequently altered airports.py

--- Fly/airports.py
+++ Fly/airports.py
@@ -1,4 +1,4 @@
-from math import sqrt
+from math import sqrt, floor
 
 class Airport(object):
 	"""Representation of a city."""
@@ -7,39 +7,63 @@ class Airport(object):
 		self.location = location	#(x, y) location on map
 		self.connects = connects	#string list of connected airports
 
+	#toString method for an Airport object.
+	def __str__(self):
+		s = "Airport " + self.name + " is located at"
+		s += " " + str(self.location) + " and is connected to"
+		s+= " the following ariports: "
+		for connect in self.connects:
+			s += "\n~" + connect + ","
+
+		#remove the extra comma
+		return s[:-1]
+
 
 class Airports(object):
 	"""Represenation of Cities"""
 	def __init__(self):
-		self.airports = []	#list of airport names
+		self.airports = []	#list of airport objects
 		self.distances = {} #hashmap of distances between airports
 
 	#Function that adds a new_airport to the list of airports. 
-	def add_airport(self, name, location, connections):
-		if name not in self.airports:
-			new_airport = Airport(name, location, connections)
+	def add_airport(self, new_airport):
+		if new_airport not in self.airports:
 			self.airports.append(new_airport)
+
+	#This method will call on add_aiport to add the 
+	#parameter list of Airport objects.
+	def add_airports(self, airports):
+		for airport in airports:
+			self.add_airport(airport)
 	
+	#This method, given an Airport name, searches for
+	#the corresponding Airport object; 
+	#returns None if not found. 
+	def search_airports(self, name):
+		for i in range(len(self.airports)):
+			if self.airports[i].name == name:
+				return i
+		return None
+
 	#This function finds the distances between 
-	#airports a and b.
+	#the Airport objects airport_a and airport_b.
 	#Note: We're only interested in distance here; we'll have
 	#a seperate function when actually traveling to/from airports.
 	def get_distance(self, airport_a, airport_b):	
 		#Check if we already know the distance
-		if (airport_a, airport_b) in self.distance:
-			return self.distance[(airport_a, airport_b)]
+		if (airport_a, airport_b) in self.distances:
+			return self.distances[(airport_a, airport_b)]
 
-		if (airport_b, airport_a) in self.distance:
-			return self.distance[(airport_b, airport_a)]
+		if (airport_b, airport_a) in self.distances:
+			return self.distances[(airport_b, airport_a)]
 
 		#Find the indicies of the airports;
 		#return -1 (an invalid distance) if either
 		#airports do not exist in the list of airports.
-		try:
-			index_a = self.airports.index(airport_a)
-			index_b = self.airports.index(airport_b)
-		except ValueError as e:
-			return -1	#Error case
+		index_a = self.search_airports(airport_a)
+		index_b = self.search_airports(airport_b)
+		if index_a == None or index_b == None:
+			return -1 #error case
 		else:
 			#(x, y) coords of both airports
 			(x_a, y_a) = self.airports[index_a].location
@@ -47,7 +71,7 @@ class Airports(object):
 
 			#Apply distance formula: sqrt[ (x2 - x1)^2 + (y2 - y1)^2]
 			distance = ((x_a - x_b)**2) + ((y_a - y_b)**2)
-			distance = sqrt(distance)
+			distance = floor(sqrt(distance))
 
 			#Add the new distance to the dictionary of distances
 			self.distances[(airport_a, airport_b)] = distance
--- Fly/test_airports.py
+++ Fly/test_airports.py
@@ -1,14 +1,24 @@
 import unittest
 from airports import Airport, Airports
+#import pdb
 
 class TestAirportMethods(unittest.TestCase):
 	"""Testing the methods from airports.py"""
 
-	#create some default airports to work with
+	#Create some default airports to work with.
+	#Create an airport system (from class Airports) to be used
+	#in the later methods. 
 	def setUp(self):
 		self.LAX = Airport('LAX', (200, 20), ['Eugene'])
 		self.Eugene = Airport('Eugene', (100, 30), ['LAX'])	#oregon
 		self.SEA = Airport('Seattle-Tacoma', (20, 50), ['Eugene'])	#washington
+		self.system = Airports()
+
+	#Test the toString method for an Airport object.
+	def test_str(self):
+		target = "Airport LAX is located at (200, 20) and is connected"
+		target += " to the following ariports: \n~Eugene"
+		self.assertEqual((str(self.LAX)), target)
 
 	#verify that airports have the correct informatioin 
 	#concerning name, location, and connected airports.
@@ -17,6 +27,34 @@ class TestAirportMethods(unittest.TestCase):
 		self.assertEqual(self.LAX.name, 'LAX')
 		self.assertEqual(self.Eugene.location, (100, 30))
 		self.assertEqual(self.SEA.connects, ['Eugene'])
+
+	#this method tests adding different airports to our
+	#airport system. 
+	def test_add_airport(self):
+		self.system.add_airport(self.LAX)
+		self.assertEqual(self.system.airports[0].name, 'LAX')
+
+	#this method tests the method add_airports which, when 
+	#given a list, adds each Airport object in the list to 
+	#system.airports.
+	def test_add_airports(self):
+		lst = [self.Eugene, self.SEA]
+		self.system.add_airports(lst)
+		self.system.add_airport(self.LAX)
+		self.assertEqual(self.system.airports[0].name, 'Eugene')
+		self.assertEqual(self.system.airports[1].name, 'Seattle-Tacoma')
+		self.assertEqual(self.system.airports[2].name, 'LAX')
+
+	#This method tests the get_distance() method.
+	#Note: distance formula: sqrt[ (x2 - x1)^2 + (y2 - y1)^2]
+	def test_get_distance(self):
+		lst = [self.Eugene, self.SEA, self.LAX]
+		self.system.add_airports(lst)
+		self.assertEqual(self.system.get_distance('Eugene', 'LAX'), 100)
+
+
+
+
 
 
 
